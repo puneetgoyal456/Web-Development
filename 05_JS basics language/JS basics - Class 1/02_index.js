5832f0826f716b358c149028b50c1b7b82f41b22
Puneet Goyal <kumar.goyal.puneet@gmail.com>
2025-03-20 23:54:00 +0530

    basics of javascript

---
+++ 05_JS basics language/JS basics - Class 1/02_index.js
@@ -0,0 +1,88 @@
+// // line below is used to print on the console window
+// console.log("Namaste Dunia Version 4");
+
+// let a = 5;
+// console.log(a);
+
+// let b = "Love";
+// console.log(b);
+
+// let c = true;
+// console.log(c);
+
+// const num1 = 12;
+// // num1 = 13;   //   ->  note change due to const variable 
+
+// let FirstName = 'Puneet';
+// console.log(FirstName);
+
+// FirstName = 3;
+// console.log(FirstName);
+
+// let person{
+//     let firstName : 'puneet';
+//     let age : 24;
+// };
+
+// console.log(person.age);
+// console.log(person[age]);
+
+// let names = ["qwer", "wertyu", "sdfghj", "xcvbnm"];
+// console.log(names[0]);
+
+// names[4] = "sdfghjk";
+// console.log(names[4]);
+
+// names[1] = 1;
+// console.log(names[1]);
+
+// console.log(2 === 3);
+
+// let age = 27;
+// let status = (age >= 18) ? "Yes Vote" : "No Vote";
+// console.log(status);
+
+// let marks = 98;
+// if (marks >= 90) {
+//   console.log("A");
+// } else if (marks >= 80) {
+//   console.log("B");
+// } else if (marks >= 70) {
+//   console.log("C");
+// } else if (marks >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("E");
+// }
+
+// let num = 2;
+
+// switch (num) {
+//   case 1:
+//     console.log("A");
+//     break;
+//   case 2:
+//     console.log("B");
+//     break;
+//   case 3:
+//     console.log("C");
+//     break;
+//   default:
+//     console.log("D");
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log("puneet " + i);
+// }
+
+// let x = 1;
+// while (x <= 5) {
+//   console.log(x);
+//   x++;
+// }
+
+// let y = 1;
+// do {
+//   console.log(y);
+//   y++;
+// } while (y <= 10);
